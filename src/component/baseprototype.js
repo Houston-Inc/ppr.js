69ae4df1e3b49ff7a4277319b23f817944f93f37
Niko Salminen <niko@salminen.me>
2016-11-10 12:23:25 +0200

    Create instances directly from component and page prototypes

--- src/component/baseprototype.js
+++ src/component/baseprototype.js
@@ -48,8 +48,8 @@
     /**
      * Create and return a new component based on this one
      */
-    component: function(obj) {
-      return this.objectAssign({}, this, obj);
+    createComponent: function(obj) {
+      return ObjectUtils.assign({}, this, obj);
     },
 
     /**
@@ -192,30 +192,6 @@
      */
     setModuleMessages: function(messages) {
       this.messages = messages;
-    },
-
-    /**
-     * Legacy browser compatible replacement for Object.assign
-     * From: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
-     */
-    objectAssign: function(target) {
-      // We must check against these specific cases.
-      if (target === undefined || target === null) {
-        throw new TypeError('Cannot convert undefined or null to object');
-      }
-
-      var output = Object(target);
-      for (var index = 1; index < arguments.length; index++) {
-        var source = arguments[index];
-        if (source !== undefined && source !== null) {
-          for (var nextKey in source) {
-            if (source.hasOwnProperty(nextKey)) {
-              output[nextKey] = source[nextKey];
-            }
-          }
-        }
-      }
-      return output;
     }
   };
 });
